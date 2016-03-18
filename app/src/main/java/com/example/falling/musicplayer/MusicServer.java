337e055fb7bd65c5ae7fe2e801c219c82fd94f19
falling <zyz635857938@gmail.com>
2016-03-18 15:51:52 +0800

    添加播放服务。

---
+++ app/src/main/java/com/example/falling/musicplayer/MusicServer.java
@@ -0,0 +1,58 @@
+package com.example.falling.musicplayer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.support.annotation.Nullable;
+
+import java.io.IOException;
+
+
+public class MusicServer extends Service {
+    public static final int START = 0;
+    public static final int PAUSE = 1;
+    public static final int CONTINUE = 3;
+    private static String musicUrl;
+    private static MediaPlayer mMediaPlayer = new MediaPlayer();
+
+    Messenger mMessenger = new Messenger(new MusicPlayerHandler());
+
+    static class MusicPlayerHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            // 处理消息
+            switch (msg.what) {
+                case START:
+                    musicUrl = msg.getData().getString("musicUrl");
+                    try {
+                        mMediaPlayer.stop();
+                        mMediaPlayer.setDataSource(musicUrl);
+                        mMediaPlayer.prepare();
+                        mMediaPlayer.start();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case PAUSE:
+                    if (mMediaPlayer.isPlaying()) {
+                        mMediaPlayer.pause();
+                    }
+                    break;
+                case CONTINUE:
+                    mMediaPlayer.pause();
+            }
+        }
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mMessenger.getBinder();
+    }
+}
